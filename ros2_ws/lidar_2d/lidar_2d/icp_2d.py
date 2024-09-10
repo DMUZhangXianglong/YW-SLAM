e487523ceb22b21bdc0b42bcd5f93d7f069c313c
DMUZhangXianglong <347913076@qq.com>
2024-09-10 16:38:45 +0800

    update code

--- ros2_ws/lidar_2d/lidar_2d/icp_2d.py
+++ ros2_ws/lidar_2d/lidar_2d/icp_2d.py
@@ -1,7 +1,7 @@
 '''
 * @Author: DMU zhangxianglong
 * @Date: 2024-09-09 13:24:58
-* @LastEditTime: 2024-09-10 10:12:51
+* @LastEditTime: 2024-09-10 16:16:22
 * @LastEditors: DMU zhangxianglong
 * @FilePath: /yw_ws/YW-SLAM/ros2_ws/lidar_2d/lidar_2d/icp_2d.py
 * @Description: 
@@ -12,6 +12,7 @@ from sensor_msgs.msg import LaserScan
 import cv2 
 import numpy as np
 import sophuspy as sp
+
 
 
 class LaserScanSubscriber(Node):
@@ -53,8 +54,8 @@ class LaserScanSubscriber(Node):
                 continue
             
             psubmap = sp.SE2().inverse() * (sp.SE2() * np.array([x, y]))
-            image_x = int(psubmap[0] * 50.0 + 800 / 2)
-            image_y = int(psubmap[1] * 50.0 + 800 / 2)
+            image_x = int(psubmap[0] * 30.0 + 800 / 2)
+            image_y = int(psubmap[1] * 30.0 + 800 / 2)
             
 
             if (image_x >= 0 and image_x < image.shape[0] and image_y >= 0 and image_y < image.shape[1]):
@@ -63,7 +64,46 @@ class LaserScanSubscriber(Node):
         
         # 显示画面   
         cv2.waitKey(20) 
+
+# 实现2d icp
+class Icp2d():
+    # 构造函数
+    def __init__(self):
+        self.source = None
+        self.target = None
+    
+    def SetSource(self, source):
+        self.source = source
+    
+    def SetTarget(self, target):
+        self.target = target
+        
+    def BuildTargetKdTree(self):
+        if(self.target == None):
+            return
+        
+        for i in range(len(self.target)):
+            if(self.target.ranges[i] < self.target.range_min or self.target.ranges[i] > self.target.range_max):
+                continue
             
+            real_angle = self.target.angle_min + i * self.target.angle_increment
+            
+            p = Point2D
+            p.x = self.target.ranges[i] * np.cos(real_angle)
+            p.y = self.target.ranges[i] * np.sin(real_angle)
+            
+            
+            
+         
+            
+# 2d 点类
+class Point2D:
+    def __init__(self, x=0.0, y=0.0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Point2D(x={self.x}, y={self.y})"
 
 
 def main(args=None):
